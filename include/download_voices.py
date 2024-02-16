3a61ba1e20d48d4c3cadac5aea1cc95053fee3fb
Tim Leverentz <timlev@gmail.com>
2024-02-15 21:42:29 -0600

    Tried to add download of piper too, but it doesn't seem to work

--- include/download_voices.py
+++ include/download_voices.py
@@ -1,5 +1,19 @@
 import subprocess, os
 
+
+piper_url = "https://github.com/rhasspy/piper/releases/download/v1.2.0/piper_amd64.tar.gz"
+
+os.chdir("/home/levtim/Downloads")
+if os.path.exists("piper"):
+	os.chdir("piper")
+else:
+	os.mkdir("piper")
+if os.path.exists("piper"):
+	os.chdir("piper")
+else:
+	subprocess.run(["wget", piper_url, "&&", "tar", "-xzvf", "piper_amd64.tar.gz"], shell=True)
+	os.chdir("piper")
+
 example = "https://huggingface.co/rhasspy/piper-voices/blob/v1.0.0/en/en_US/amy/low/en_US-amy-low.onnx"
 
 
